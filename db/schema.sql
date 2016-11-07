5d2d2a03e93f87a868277b5abc651410f03412c1
Matt Bedell <matt@bedell.co>
2016-11-07 16:14:38 -0500

    imported shema

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS task;
 
-CREATE TABLE tasks (
+CREATE TABLE task (
   id SERIAL PRIMARY KEY,
   name VARCHAR(64) NOT NULL,
   description VARCHAR(255),
   completed BOOLEAN NOT NULL DEFAULT FALSE,
   deleted BOOLEAN NOT NULL DEFAULT FALSE,
-  date_created TIMESTAMP  DEFAULT FALSE DEFAULT NOW(),
+  date_created TIMESTAMP NOT NULL DEFAULT NOW(),
   date_completed TIMESTAMP,
   date_deleted TIMESTAMP
 );
 -- Create an index
-CREATE INDEX 'name_idx'
-ON tasks (name);
+CREATE INDEX name_idx
+ON task (name);

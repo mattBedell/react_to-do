6d2e0ca43c65e0c08ade3505df03c422737ad07c
Matt Bedell <matt@bedell.co>
2016-11-18 12:59:33 -0500

    save

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,16 +7,77 @@ import TaskList  from '../TaskList';
 import './App.css';
 import './GA_gear.png';
 
+class Task {
+  constructor(name, desc) {
+    this.name = name;
+    this.desc = desc;
+  }
+}
+
+
 export default class App extends React.Component {
 
   constructor(props) {
     super();
 
     this.state = {
-      tasks: {},
+      tasks: {
+        newItems: []
+      },
+      taskName: undefined,
+      taskDescription: undefined,
     };
+    this.addTask = this.addTask.bind(this);
   }
+  addTask(task) {
+    if(task.name === '') {
+    } else {
+      fetch('http://localhost:3000/tasks', {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(task)
+      }).then( () => {
+        let addToItems = this.state.tasks.newItems;
+        addToItems.push({...task})
+        this.setState({
+          tasks: {
+            newItems: addToItems
+          }
+        })
+      })
+      .catch((err) => {
+        console.log(`POST FETCH ERROR ${err}`);
+      })
+    }
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    let taskName = e.target.taskName.value;
+    let taskDesc = e.target.taskDesc.value;
 
+    const newTask = new Task(taskName, taskDesc);
+
+    this.addTask(newTask);
+
+
+  }
+  componentDidMount() {
+    fetch('http://localhost:3000/tasks')
+    .then((r) => r.json())
+    .then((data) => {
+      this.setState({
+        tasks: {
+          newItems: data
+        }
+      })
+    })
+    .catch((err) => {
+      console.log(`GET FETCH ERROR ${err}`);
+    })
+  }
   render() {
     return (
       <container>
@@ -26,13 +87,17 @@ export default class App extends React.Component {
         <main className="container">
           <section className="jumbotron">
             <h1>Task Manager</h1>
-            <TaskForm />
+            <TaskForm
+              preventDef={(event) => this.handleSubmit(event)}
+            />
           </section>
           {/* to do lists */}
           <section className="row">
             <article className="col-md-4">
               <h3>Open Items</h3>
-              <TaskList />
+              <TaskList
+                newTasks={this.state.tasks.newTasks}
+               />
             </article>
 
             <article className="col-md-4">
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
 const TaskForm = (props) => (
-  <form className="form-inline" onSubmit={(e)=>console.log(e)}>
+  <form className="form-inline" onSubmit={props.preventDef}>
 
     <div className="form-group">
       <label className="sr-only" htmlFor="taskName">Task Name</label>
-      <input type="text" className="form-control input-lg" name="taskName" placeholder="Task Name" />
+      <input
+
+        type="text"
+        className="form-control input-lg"
+        name="taskName"
+        placeholder="Task Name" />
     </div>
 
     <div className="form-group">
       <label className="sr-only" htmlFor="taskDesc">Task Description</label>
-      <input type="text" className="form-control input-lg" name="taskDesc" placeholder="Task Description" />
+      <input
+        type="text"
+        className="form-control input-lg"
+        name="taskDesc"
+        placeholder="Task Description"
+      />
     </div>
-    <button type="submit" className="btn btn-danger btn-lg">Add Task</button>
+    <button
+      onChange={props.handleSubmit}
+      type="submit"
+      className="btn btn-danger btn-lg"
+      >Add Task</button>
   </form>
 );
 
